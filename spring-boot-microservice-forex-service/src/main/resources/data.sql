ce4a293ba97e73301552564f3829febfc18634e0
a-farsi <abdelhak.farsi@gmail.com>
2020-02-21 01:44:43 +0100

    example microservice with spring-boot, Ribbon, Feign and eurika

---
+++ spring-boot-microservice-forex-service/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10001,'USD','EUR',0.93,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10002,'EUR','USD',1.08,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10003,'CHF','EUR',0.94,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10004,'CAD','EUR',0.70,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10005,'EUR','CAD',1.43,0);
